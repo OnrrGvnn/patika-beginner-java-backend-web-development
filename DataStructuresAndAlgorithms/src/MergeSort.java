a27c247edb594f0cc239393afc86263cc6f78777
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-09-29 17:33:38 +0300

    MergeSort project finish

---
+++ DataStructuresAndAlgorithms/src/MergeSort.java
@@ -0,0 +1,19 @@
+public class MergeSort {
+    /*
+            [16,21,11,8,12,22]
+
+        1- Write the steps of the above array according to the type of sort.
+
+            [16,21,11,8,12,22]
+            [16,21,11] [8,12,22]
+            [16] [21,11] [8] [12,22]
+            [16] [11,21] [8] [12,22]
+            [11,16,21] [8,12,22]
+            [8,11,12,16,21,22]
+
+        2- Write the Big-O notation.
+
+            O(nlog(n))
+
+     */
+}
