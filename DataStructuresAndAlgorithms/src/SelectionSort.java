29ce98250884119f89e1bca53aa7f02e06b36b8b
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-09-29 16:55:01 +0300

    SelectionSort project finish

---
+++ DataStructuresAndAlgorithms/src/SelectionSort.java
@@ -0,0 +1,30 @@
+public class SelectionSort {
+    /*
+
+        [22,27,16,2,18,6]
+
+    1- Write the steps of the given array according to the type of sort.
+
+        [22,27,16,2,18,6]
+        [2,27,16,22,18,6]
+        [2,6,16,22,18,27]
+        [2,6,16,18,22,27]
+
+    2- Write the Big-O notation.
+
+        O(n^2)
+
+    3- Time Complexity: After the array is sorted, which of the following cases does the number 18 fall into? Write your answer.
+
+        [2,6,16,18,22,27] -> 18 is in the middle, Average Case.
+
+    4- Write the first 4 steps of the array [7,3,5,8,2,9,4,15,6] according to Selection Sort.
+
+        [7,3,5,8,2,9,4,15,6]
+        [2,3,5,8,7,9,4,15,6]
+        [2,3,4,8,7,9,5,15,6]
+        [2,3,4,5,7,9,8,15,6]
+        [2,3,4,5,6,9,8,15,7]
+
+     */
+}
